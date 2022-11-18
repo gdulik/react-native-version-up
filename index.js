bbe3a8b0f6d0ccecbbf68f5879f3b020da71b6b0
Tomáš Gdula <tomas.gdula@gmail.com>
2022-11-18 14:42:48 +0100

    Refactor whole package

--- index.js
+++ index.js
@@ -12,11 +12,8 @@ const pathToRoot = process.cwd();
 const pathToPackage = argv.pathToPackage || `${pathToRoot}/package.json`;
 const info = helpers.getPackageInfo(pathToPackage);
 
-const pathToPlist = argv.pathToPlist || `${pathToRoot}/ios/${info.name}/Info.plist`;
+const pathToPbxproj = argv.pathToPbxproj || `${pathToRoot}/ios/${info.name}.xcodeproj/project.pbxproj`;
 const pathToGradle = argv.pathToGradle || `${pathToRoot}/android/app/build.gradle`;
-// handle case of several plist files
-const pathsToPlists = Array.isArray(pathToPlist) ? pathToPlist : [pathToPlist];
-
 
 // getting next version
 const versionCurrent = info.version;
@@ -27,23 +24,32 @@ let patch = helpers.version(versions[2], argv.patch, argv.major || argv.minor);
 const version = `${major}.${minor}.${patch}`;
 
 // getting next build number
-const buildCurrent = helpers.getBuildNumberFromPlist(pathsToPlists[0]);
-const build = buildCurrent + 1;
+const androidBuildCurrent = helpers.getBuildNumberFromGradle(pathToGradle);
+let androidBuild = androidBuildCurrent + 1;
+const iosBuildCurrent = helpers.getBuildNumberFromPbxproj(pathToPbxproj);
+let iosBuild = 1;
+if(argv.build) {
+  iosBuild = iosBuildCurrent + 1;
+}
 
 // getting commit message
 const messageTemplate = argv.m || argv.message || 'release ${version}: increase versions and build numbers';
-const message = messageTemplate.replace('${version}', version);
+const message = messageTemplate.replace('${version}', iosBuild > 1 ? `${version}-${iosBuild}` : version);
 
 log.info('\nI\'m going to increase the version in:');
 log.info(`- package.json (${pathToPackage});`, 1);
-log.info(`- ios project (${pathsToPlists.join(', ')});`, 1);
+log.info(`- ios project (${pathToPbxproj});`, 1);
 log.info(`- android project (${pathToGradle}).`, 1);
 
 log.notice(`\nThe version will be changed:`);
-log.notice(`- from: ${versionCurrent} (${buildCurrent});`, 1);
-log.notice(`- to:   ${version} (${build}).`, 1);
+log.notice(`- on iOS`, 1)
+log.notice(`- from: ${versionCurrent} (${iosBuildCurrent});`, 2);
+log.notice(`- to:   ${version} (${iosBuild}).`, 2);
+log.notice(`- on android`, 1)
+log.notice(`- from: ${versionCurrent} (${androidBuildCurrent});`, 2);
+log.notice(`- to:   ${version} (${androidBuild}).`, 2);
 
-if (version === versionCurrent) {
+if (version === versionCurrent && (iosBuild === iosBuildCurrent || androidBuild === androidBuildCurrent)) {
   log.warning('\nNothing to change in the version. Canceled.');
   process.exit();
 }
@@ -72,14 +78,12 @@ const update = chain.then(() => {
 }).then(() => {
   log.info('Updating version in xcode project...', 1);
 
-  pathsToPlists.forEach(pathToPlist => {
-    helpers.changeVersionAndBuildInPlist(pathToPlist, version, build);
-  });
-  log.success(`Version and build number in ios project (plist file) changed.`, 2);
+  helpers.changeVersionAndBuildInPbxproj(pathToPbxproj, version, iosBuild);
+  log.success(`Version and build number in ios project (pbxproj file) changed.`, 2);
 }).then(() => {
   log.info('Updating version in android project...', 1);
 
-  helpers.changeVersionAndBuildInGradle(pathToGradle, version, build);
+  helpers.changeVersionAndBuildInGradle(pathToGradle, version, androidBuild);
   log.success(`Version and build number in android project (gradle file) changed.`, 2);
 });
 
@@ -88,14 +92,18 @@ const commit = update.then(() => {
   log.info('I want to make a commit with message:', 1);
   log.info(`"${message}"`, 2);
   log.info(`I want to add a tag:`, 1);
-  log.info(`"v${version}"`, 2);
+  if(iosBuild > 1) {
+    log.info(`"v${version}-${iosBuild}"`, 2);
+  } else {
+    log.info(`"v${version}"`, 2);
+  }
 
   const question = log.info(`Do you allow me to do this? [y/n] `, 1, true);
   const answer = readlineSync.question(question).toLowerCase();
   if (answer === 'y') {
-    return helpers.commitVersionIncrease(version, message, [
+    return helpers.commitVersionIncrease(version, iosBuild, message, [
       pathToPackage,
-      ...pathsToPlists,
+      pathToPbxproj,
       pathToGradle
     ]).then(() => {
       log.success(`Commit with files added. Run "git push".`, 1);
