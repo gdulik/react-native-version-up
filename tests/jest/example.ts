5d765f59d4297c5037aeed328659aebec5974401
Tomáš Gdula <tomas.gdula@gmail.com>
2022-11-26 11:27:06 +0100

    Add jest tests

--- tests/jest/example.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Just an example of simple test
- */
-test('example test of 1 + 1', () => {
-  expect(1 + 1).toBe(2);
-});
-
---
+++ tests/jest/index.ts
@@ -0,0 +1,119 @@
+import helpers from '../../src/utils/helpers';
+import log from '../../src/utils/log';
+
+const pathToRoot = process.cwd();
+const pathToPackage = `${pathToRoot}/tests/jest/test.package.json`
+const pathToGradle = `${pathToRoot}/tests/jest/test.build.gradle`
+const pathToPbxproj = `${pathToRoot}/tests/jest/test.project.pbxproj`
+
+describe('Helpers module', () => {
+  test('Versions splitter', () => {
+    expect(helpers.versions('1.2.3')).toEqual(['1','2','3'])
+  })
+
+  test('Version not increased', () => {
+    expect(helpers.version('1', false)).toBe(1)
+  })
+
+  test('Version increased', () => {
+    expect(helpers.version('1', true)).toBe(2)
+  })
+
+  test('Version reset', () => {
+    expect(helpers.version('1', false, true)).toBe(0)
+  })
+
+  test('Get package info', () => {
+    expect(helpers.getPackageInfo(pathToPackage)).toEqual({
+      "name": "testapp",
+      "version": "1.4.2",
+      "scripts": {
+        "android": "react-native run-android",
+        "ios": "react-native run-ios"
+      },
+      "dependencies": {
+        "react": "17.0.1",
+        "react-native": "0.64.4"
+      },
+      "devDependencies": {
+        "@gdulik/react-native-version-up": "1.1.0"
+      }
+    })
+  })
+
+  test('Get build number from gradle', () => {
+    expect(helpers.getBuildNumberFromGradle(pathToGradle)).toBe(27)
+  })
+
+  test('Get build number from pbxproj', () => {
+    expect(helpers.getBuildNumberFromPbxproj(pathToPbxproj)).toBe(1)
+  })
+
+  test('Change version in package', () => {
+    helpers.changeVersionInPackage(pathToPackage, '1.4.3')
+
+    expect(helpers.getPackageInfo(pathToPackage)).toEqual({
+      "name": "testapp",
+      "version": "1.4.3",
+      "scripts": {
+        "android": "react-native run-android",
+        "ios": "react-native run-ios"
+      },
+      "dependencies": {
+        "react": "17.0.1",
+        "react-native": "0.64.4"
+      },
+      "devDependencies": {
+        "@gdulik/react-native-version-up": "1.1.0"
+      }
+    });
+
+    helpers.changeVersionInPackage(pathToPackage, '1.4.2');
+  })
+
+  test('Change version and build number in gradle', () => {
+    helpers.changeVersionAndBuildInGradle(pathToGradle, '1.4.3', 28);
+
+    expect(helpers.getBuildNumberFromGradle(pathToGradle)).toBe(28);
+
+    helpers.changeVersionAndBuildInGradle(pathToGradle, '1.4.2', 27);
+  })
+
+  test('Change version and build number in pbxproj', () => {
+    helpers.changeVersionAndBuildInPbxproj(pathToPbxproj, '1.4.3', 2);
+
+    expect(helpers.getBuildNumberFromPbxproj(pathToPbxproj)).toBe(2);
+
+    helpers.changeVersionAndBuildInPbxproj(pathToPbxproj, '1.4.2', 1);
+  })
+})
+
+describe('Log module', () => {
+  test('Echo with level', () => {
+    expect(log.echo('Test message', 1, null, true)).toBe('  Test message')
+  })
+
+  test('Error log', () => {
+    expect(log.error('Test message', 0, true)).toBe('\u001B[31mTest message\u001B[39m')
+  })
+
+  test('Success log', () => {
+    expect(log.success('Test message', 0, true)).toBe('\u001B[32mTest message\u001B[39m')
+  })
+
+  test('Warning log', () => {
+    expect(log.warning('Test message', 0, true)).toBe('\u001B[33mTest message\u001B[39m')
+  })
+
+  test('Notice log', () => {
+    expect(log.notice('Test message', 0, true)).toBe('\u001B[34mTest message\u001B[39m')
+  })
+
+  test('Info log', () => {
+    expect(log.info('Test message', 0, true)).toBe('Test message')
+  })
+
+  test('Line log', () => {
+    expect(log.line(true)).toBe('')
+  })
+})
